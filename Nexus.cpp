b66f96669d33fe63f29353af58add435d2d7ee5b
천 정 <wjdcjs3093@naver.com>
2019-10-29 10:11:58 +0900

    tower 작업 하다 만것들.. 읽기 제외

--- Nexus.cpp
+++ Nexus.cpp
@@ -20,17 +20,27 @@ HRESULT CNexus::Initialize()
 	CloneMesh(GetDevice(), L"Zealot", &m_pAnimationCtrl);
 	if (!m_pAnimationCtrl) return S_FALSE;
 
-	
+	UpdateWorldMatrix();
+	m_pAnimationCtrl->SetAnimationSet("Default_Action");
 
-	return E_NOTIMPL;
+	m_Info.vLook = D3DXVECTOR3(0, 0, -1.0f);
+	m_Info.vDir = D3DXVECTOR3(0, 0, -1.0f);
+	m_Info.vPos = D3DXVECTOR3(0, 0, 0);
+	fill(&m_fAngle[0], &m_fAngle[ANGLE_END], 0);
+
+	return S_OK;
 }
 
 void CNexus::Progress()
 {
+	CTower::UpdateWorldMatrix();
+	m_pAnimationCtrl->FrameMove(L"Udyr", g_fDeltaTime);
 }
 
 void CNexus::Render()
 {
+	SetTransform(D3DTS_WORLD, &m_Info.matWorld);
+	Mesh_Render(GetDevice(), L"Udyr");
 }
 
 void CNexus::Release()
