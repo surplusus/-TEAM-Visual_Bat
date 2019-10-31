89f5a95d2b21e9e5035c046999e27c59035e7f04
Guhyun <moing6862@naver.com>
2019-10-31 19:41:35 +0900

    일단 저장

--- Behavior.cpp
+++ Behavior.cpp
@@ -1,2 +1,30 @@
 #include "BaseInclude.h"
 #include "Behavior.h"
+#include ""
+
+inline void BehaviorTree::Behavior::SetUp(Node & node) {
+	Release();
+	m_pNode = &node;
+	m_pTask = node.Init();
+}
+
+void BehaviorTree::Behavior::Release()
+{
+	if (m_pTask == nullptr)
+		return;
+	assert(m_eStatus != RUNNING);
+	m_pNode->Release(m_pTask);
+	m_pTask = nullptr;
+}
+
+BehaviorTree::Status BehaviorTree::Behavior::Tick()
+{
+	if (m_eStatus == INVALID)
+		m_pTask->OnInit();
+
+	m_eStatus = m_pTask->Progress();
+
+	if (m_eStatus != RUNNING)
+		m_pTask->OnRelease(m_pTask);
+	return m_eStatus;
+}
--- Behavior.h
+++ Behavior.h
@@ -1,16 +1,63 @@
 #pragma once
-class CBehavior
+#include <memory>
+#include <cassert>
+namespace BehaviorTree
 {
-public:
-	virtual ~CBehavior();
-};
+	class Node;
+	class Task;
 
-class CBehavior_Idle
-{
-public:
-	CBehavior_Idle();
-	~CBehavior_Idle();
+	enum Status
+	{
+		INVALID,
+		SUCCESS,
+		FAILURE,
+		RUNNING
+	};
 
-private:
+	class Node
+	{
+	public:
+		virtual Task* Init() = 0;
+		virtual void Release() = 0;
+		virtual ~Node() {}
+	};
 
-};
+	class Task
+	{
+	public:
+		Task(Node& node)
+			: m_pNode(&node) {}
+		virtual ~Task() {}
+		virtual Status Progress() = 0;
+		virtual void OnInit() {}
+		virtual void OnRelease(Status) {}
+	protected:
+		Node* m_pNode;
+	};
+
+	class Behavior
+	{
+	protected:
+		Task* m_pTask;
+		Node* m_pNode;
+		Status m_eStatus;
+	public:
+		Behavior()
+			: m_pTask(nullptr)
+			, m_pNode(nullptr)
+			, m_eStatus(INVALID) {}
+		Behavior(Node& node)
+			: m_pTask(nullptr)
+			, m_pNode(nullptr)
+			, m_eStatus(INVALID) {
+			SetUp(node);
+		}
+		~Behavior() {
+			m_eStatus = INVALID;
+			Release();
+		}
+		void SetUp(Node& node);
+		void Release();
+		Status Tick();
+	};
+}
